7b09aac4e0444c16615e1c54d6e96dcf1803c1a9
Carl-l-l <carlleeladefoged@gmail.com>
2025-05-30 16:13:53 +0200

    Very basic logging implemented

--- app/app.py
+++ app/app.py
@@ -1,12 +1,12 @@
 import joblib
 import pandas as pd
+import logging
 from flask import Flask, request, jsonify
-
-
 
 ready_flag = False # Track when the app is ready for requests during init
 
-# TODO: Implement logger logic
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename="app/app.log", level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 app = Flask(__name__)
 
@@ -28,11 +28,12 @@ def post_predict():
     Returns a prediction based on the loaded model.
     """
     if not ready_flag:
+        logger.error("Service not ready to handle requests")
         return jsonify({"error": "Service not ready"}), 503
 
     data = request.get_json()
     
-    # Request validate
+    # Request validation
     # TODO: Add attribute in RealEstateModel to get required features (yuck, hard-coded...)
     # X1 transaction date,X2 house age,X3 distance to the nearest MRT station,X4 number of convenience stores,X5 latitude,X6 longitude
     required_features = [
@@ -45,6 +46,7 @@ def post_predict():
     ]
     missing_features = [feature for feature in required_features if feature not in data]
     if missing_features:
+        logger.error(f"Missing features in request: {', '.join(missing_features)}")
         return jsonify({"error": f"Missing features: {', '.join(missing_features)}"}), 400
     
     data_df = pd.DataFrame([data])
@@ -52,6 +54,8 @@ def post_predict():
     # Make prediction
     prediction = model.predict(data_df)
     
+    logger.info(f"Received data: {data}, Prediction: {prediction[0]}")
+
     return jsonify({
         "prediction": prediction[0]
     }), 200
@@ -60,7 +64,9 @@ def post_predict():
 if __name__ == '__main__':
     try:
         ready_flag = True 
+        logger.info("Starting the Flask app...")
         app.run(port=8000, debug=True)
+        logger.info("Flask app started successfully.")
     except Exception as e:
         print(f"Error starting the app: {e}")
         ready_flag = False
