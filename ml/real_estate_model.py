e1284a314cedd8795c6efae3adf89fcc0540d06b
Carl-l-l <carlleeladefoged@gmail.com>
2025-05-30 16:07:28 +0200

    Added required_columns attribute to REModel, and set index_col to 0

--- ml/real_estate_model.py
+++ ml/real_estate_model.py
@@ -7,13 +7,17 @@ import joblib
 
 class RealEstateModel:
     def __init__(self, data_path):
-        self.data = pd.read_csv(data_path)
+        self.data = pd.read_csv(data_path, index_col=0) # Set index_col to 0 to avoid using the first column as a feature ("No")
         self.model = None
+        self.required_columns = []
 
     def split_data(self, target_column, test_size=0.2, random_state=42):
         X = self.data.drop(columns=[target_column])
         y = self.data[target_column]
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+        
+        self.required_columns = X.columns.tolist()  # Store the required columns for later use (input validation)
+        
         return X_train, X_test, y_train, y_test
     
     def train_model(self, X_train, y_train):
@@ -34,7 +38,10 @@ class RealEstateModel:
         return self.model.predict(X)
     
     def save_model(self, model_path):
-
+        # Save trained model
+        if self.model is None:
+            raise Exception("Please train (train_model) the model before saving.")
+        
         joblib.dump(self.model, model_path)
 
     def load_model(self, model_path):
